b9ee4708e1dec1a3b5ee8f50a78d68146a0ef773
unisanta <unisanta@santacecilia.br>
2024-10-16 22:19:18 -0300

    Página descobrir feita como rascunho

--- src/routes/Discover/Discover.jsx
+++ src/routes/Discover/Discover.jsx
@@ -1,15 +1,20 @@
 import "./Discover.css";
 import ImgDiscover from "../../assets/img-discover.png";
+import ProfileDiscover from "../../assets/pfp-discover.png";
 
 export default function Discover() {
   return (
     <section className="discover-container">
-      <div>
+      <div className="div-text">
         <p className="discover-text">Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</p>
           <input className="discover-input"></input>
       </div>
-      <div>
-        <img className="img-fixed" src={ImgDiscover} alt="" />
+      <div className="div-imgs">
+        <img className="discover-img" src={ImgDiscover} alt="" />
+        <p className="teste">
+          <img className="discover-profile" src={ProfileDiscover} alt="" />
+          <p className="discover-profile-text">Bianca Martin • 02/24</p>
+        </p>
       </div>
     </section>
   );
