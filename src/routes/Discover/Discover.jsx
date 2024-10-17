ddc4ab81b4fae60f737b4774dfa823a5203357ac
unisanta <unisanta@santacecilia.br>
2024-10-16 21:26:22 -0300

    Texto comunidade feito, resto da pagina como rascunho

--- src/routes/Discover/Discover.jsx
+++ src/routes/Discover/Discover.jsx
@@ -1,9 +1,16 @@
 import "./Discover.css";
+import ImgDiscover from "../../assets/img-discover.png";
 
 export default function Discover() {
   return (
     <section className="discover-container">
-      <h1>Discover Page</h1>
+      <div>
+        <p className="discover-text">Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</p>
+          <input className="discover-input"></input>
+      </div>
+      <div>
+        <img className="img-fixed" src={ImgDiscover} alt="" />
+      </div>
     </section>
   );
 }
