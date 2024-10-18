e9fcbaf43244b3877f07b627d2cade2b586b1216
Indalecio Olivo <indalecioolivo@gmail.com>
2024-10-18 03:59:52 -0300

    feat: cards discover created

--- src/routes/Discover/Discover.jsx
+++ src/routes/Discover/Discover.jsx
@@ -1,24 +1,29 @@
 import "./Discover.css";
-import ImgDiscover from "../../assets/img-discover.png";
-import ProfileDiscover from "../../assets/pfp-discover.png";
+import CardDiscover from "../../modules/CardDiscover/CardDiscover";
 
 export default function Discover() {
   return (
     <section className="discover-container">
       <div className="div-text">
-        <p className="discover-text">Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</p>
+        <p className="discover-text">
+          Junte-se à comunidade de inovação, inspiração e descobertas,
+          transformando experiências em conexões inesquecíveis
+        </p>
         <fieldset>
           <legend>Buscar tags</legend>
           <input></input>
         </fieldset>
       </div>
-      <div className="div-imgs">
-        <img className="discover-img" src={ImgDiscover} alt="" />
-        <p className="discover-profile-info">
-          <img className="discover-profile" src={ProfileDiscover} alt="" />
-          <p className="discover-profile-text">Bianca Martin • 02/24</p>
-        </p>
-      </div>
+      <section className="to-cards">
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+        <CardDiscover />
+      </section>
     </section>
   );
 }
