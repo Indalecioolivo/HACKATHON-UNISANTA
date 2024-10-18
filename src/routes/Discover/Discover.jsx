c4a4f76c960043feef449b433b9e387132d7e3f5
unisanta <unisanta@santacecilia.br>
2024-10-17 21:52:06 -0300

    feat: discover text and tag search done

--- src/routes/Discover/Discover.jsx
+++ src/routes/Discover/Discover.jsx
@@ -7,11 +7,14 @@ export default function Discover() {
     <section className="discover-container">
       <div className="div-text">
         <p className="discover-text">Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</p>
-          <input className="discover-input"></input>
+        <fieldset>
+          <legend>Buscar tags</legend>
+          <input></input>
+        </fieldset>
       </div>
       <div className="div-imgs">
         <img className="discover-img" src={ImgDiscover} alt="" />
-        <p className="teste">
+        <p className="discover-profile-info">
           <img className="discover-profile" src={ProfileDiscover} alt="" />
           <p className="discover-profile-text">Bianca Martin • 02/24</p>
         </p>
